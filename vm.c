1ac2ac5417307dd21b44941d6904f5753092ef38
Daniel Deiana <d.deiana1@studenti.unipi.it>
2023-11-14 21:52:17 +0100

    Added registers

---
+++ vm.c
@@ -0,0 +1,50 @@
+#include <stdlib.c> 
+
+
+/* Memory storage */
+#define MEMORY_SIZE (1 << 16)
+uint16_t memory[MEMORY_SIZE];
+
+/* Registers */
+enum
+{
+  R_R0 = 0,
+  R_R1,
+  R_R2,
+  R_R3,
+  R_R4,
+  R_R5,
+  R_R6,
+  R_R7,
+  R_PC,
+  R_FLAGS,
+  R_COUNT
+};
+
+uint16_t registers[R_COUNT];
+
+/* Instruction opcodes */
+enum
+{
+  OP_BR = 0, /* branch */
+  OP_ADD,    /* add  */
+  OP_LD,     /* load */
+  OP_ST,     /* store */
+  OP_JSR,    /* jump register */
+  OP_AND,    /* bitwise and */
+  OP_LDR,    /* load register */
+  OP_STR,    /* store register */
+  OP_RTI,    /* unused */
+  OP_NOT,    /* bitwise not */
+  OP_LDI,    /* load indirect */
+  OP_STI,    /* store indirect */
+  OP_JMP,    /* jump */
+  OP_RES,    /* reserved (unused) */
+  OP_LEA,    /* load effective address */
+  OP_TRAP    /* execute trap */
+};
+
+int main () {
+
+
+}
